88658bdb8e7d94799871b65869ad28ab31b1220b
Roni <veronicanartatez@Ronis-MacBook-Air.local>
2025-06-20 13:36:49 -0400

    added bootstrap element to see what happens

--- memory-game/src/memorygame.jsx
+++ memory-game/src/memorygame.jsx
@@ -1,1 +1,37 @@
-hi
+
+
+export default function MemoryGame() {
+
+    return(
+        <>
+
+            {/* title of page */}
+            <h1>Memory Match-Up!</h1>
+
+            {/* bar with settings. from bootstrap */}
+            <div class="card text-center">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="true" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+
+
+
+        </>
+}
